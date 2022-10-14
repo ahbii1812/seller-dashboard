379fac30404e75dfdee91a6fe221c45bb2dd1d74
Wejack <ahbii.0630@gmail.com>
2022-10-14 09:34:47 +0800

    [Enhancement] Added mobile screen label

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Grid, useMediaQuery, useTheme } from "@mui/material";
+import {
+  Box,
+  Grid,
+  styled,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import AppHeader from "./components/app-header.component";
@@ -14,6 +21,17 @@ const validationSchema = Yup.object().shape({
   description: Yup.string().required().label("Description"),
   quantity: Yup.number().required().label("Quantity"),
   declaration: Yup.boolean().oneOf([true], "Declaration must be checked"),
+});
+
+const MobileHeader = styled(Box)({
+  borderBottom: `1px solid ${color.border}`,
+  paddingLeft: "26px",
+  paddingRight: "26px",
+  paddingTop: "15px",
+  paddingBottom: "15px",
+  position: "absolute",
+  width: "100%",
+  marginTop: "5px",
 });
 
 function App() {
@@ -44,6 +62,11 @@ function App() {
         }}
       >
         <Grid container spacing={3} sx={{ marginTop: "85px" }}>
+          {isMobile && (
+            <MobileHeader>
+              <Typography>Add a product</Typography>
+            </MobileHeader>
+          )}
           <Grid
             item
             xs={isMobile ? 12 : 6}
